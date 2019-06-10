79db6602473b2538862bb2b19d5f8b6bf3e7c770
pingfei <pingfei1224@163.com>
2019-06-10 15:46:47 +0800

    添加文件webpack.prod.config.js，配置未写完，待续

---
+++ 进阶篇/demo/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <v-title title="点击"></v-title>
+    <v-button color="red" @click="handleClick">click</v-button>
+    <p>
+      <img src="./images/image.jpg" alt="">
+    </p>
+  </div>
+</template>
+<script>
+import vTitle from './title.vue'
+import vButton from './button.vue'
+export default {
+  components: {
+    vTitle,
+    vButton
+  },
+  data () {
+    return {
+      name: 'vue'
+    }
+  },
+  methods: {
+    handleClick (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+<style scoped>
+  #app {
+    color: #f50;
+    font-size: 24px;
+  }
+</style>
---
+++ 进阶篇/demo/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="handleClick" :style="styles">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '#0c6'
+    }
+  },
+  computed: {
+    styles () {
+      return {
+        background: this.color
+      }
+    }
+  },
+  methods: {
+    handleClick (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+<style scoped lang="">
+  button {
+    border: 0;
+    outline: 0;
+    color: #fff;
+    padding: 4px 8px;
+  }
+  button:active {
+    position: relative;
+    top: 1px;
+    left: 1px;
+  }
+</style>
